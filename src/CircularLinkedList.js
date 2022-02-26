7857b83917d172ece958e478f23c174361a95b9b
Artem Matiushenko <artom.matyushenko@gmail.com>
2022-02-26 21:05:22 +0200

    Created Node class
    Class will represent a single node of a circular linked list. Also added node value type validation, only string of length 1 is allowed

--- src/CircularLinkedList.js
+++ src/CircularLinkedList.js
@@ -1,1 +1,13 @@
-console.log('Hello world!');
+class Node {
+  constructor(character) {
+    const elementType = typeof character;
+    if (elementType !== 'string') {
+      throw new Error(`The value of node can\'t be typeof ${elementType}`);
+    }
+    if (character.length > 1) {
+      throw new Error('The value should be a signle character');
+    }
+    this.value = character;
+    this.next = null;
+  }
+}
